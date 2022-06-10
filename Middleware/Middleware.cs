e42455461a922dc931118bd6abb39a589e8c352d
zacxar <jakzac1@wp.pl>
2022-06-10 18:36:31 +0200

    Dodaj pliki projektów.

---
+++ Middleware/Middleware.cs
@@ -0,0 +1,26 @@
+﻿using Shyjus.BrowserDetection;
+
+namespace dotnet_7.Middleware
+{
+    public class Middleware
+    {
+        private RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IBrowserDetector detector)
+        {
+            Shyjus.BrowserDetection.IBrowser browser = detector.Browser;
+
+            if (browser.Name == BrowserNames.Edge || browser.Name == BrowserNames.EdgeChromium || browser.Name == BrowserNames.InternetExplorer)
+            {
+                //await context.Response.WriteAsync("Przegladarki Edge, EdgeChromium oraz IE nie są obsługiwane");
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+            }
+            await _next.Invoke(context);
+        }
+    }
+}
